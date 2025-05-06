c3b07f239cf4973551602698304c764cac2f2c5c
ArlleyCS <arllinho@edu.unifil.br>
2025-05-05 22:20:29 -0300

    sss
    ssss

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,16 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('baianosound', 'baianosound_user', 'oCs0Q1YPj1O8KoNVI6Zdtmrm9NNGnFvl', {
-  host: 'dpg-d0cigdumcj7s73ak5pd0-a.ohio-postgres.render.com',
-  dialect: 'postgres',
-  port: 5432, // Porta do PostgreSQL
-  logging: false, // Opcional: Desativar logs SQL
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    port: process.env.DB_PORT || 5432, // Usa 5432 se não for definido
+    logging: false, // Opcional
+  }
+);
 
 // Testar a conexão
 sequelize.authenticate()
@@ -15,13 +20,5 @@ sequelize.authenticate()
   .catch(err => {
     console.error('Não foi possível conectar ao banco de dados:', err);
   });
-  const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD, {
-      host: process.env.DB_HOST,
-      dialect: 'postgres',
-      port: process.env.DB_PORT,
-    }
-  );
-  
+
+module.exports = sequelize;
