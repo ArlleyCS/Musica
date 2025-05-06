c9fc613d24fd8d78ac83b68a4ca4adb8aa538dc7
ArlleyCS <arllinho@edu.unifil.br>
2025-05-05 23:01:51 -0300

    nm
    kk

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
-const config = require('./config.json'); // Ajuste o caminho se necessário
-const { Sequelize } = require('sequelize');
+const sequelize = require('./config/sequelize'); // ajuste o caminho se necessário
 
-// Crie uma instância do Sequelize com as configurações do arquivo config.json
-const sequelize = new Sequelize(config.development.database, config.development.username, config.development.password, {
-  host: config.development.host,
-  dialect: config.development.dialect,
-  logging: config.development.logging,
-});
-
-// Teste a conexão com o banco de dados
+// Teste a conexão com o banco (opcional se já feito no sequelize.js)
 sequelize.authenticate()
   .then(() => {
     console.log('Conexão com o banco de dados estabelecida com sucesso!');
