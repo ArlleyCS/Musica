7035d67611345f9ba7fea269997dfe2806bb0ef8
ArlleyCS <arllinho@edu.unifil.br>
2025-05-05 21:50:39 -0300

    ee
    eee

--- app.js
+++ app.js
@@ -1,51 +1,26 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const { sequelize } = require('./models');
-const methodOverride = require('method-override');
-const engine = require('ejs-mate');
+{
+  "development": {
+    "username": "baianosound_user",
+    "password": "oCs0Q1YPj1O8KoNVI6Zdtmrm9NNGnFvl",
+    "database": "baianosound",
+    "host": "dpg-d0cigdumcj7s73ak5pd0-a.ohio-postgres.render.com",
+    "dialect": "postgres",
+    "logging": false
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "baianosound_user",
+    "password": "oCs0Q1YPj1O8KoNVI6Zdtmrm9NNGnFvl",
+    "database": "baianosound",
+    "host": "dpg-d0cigdumcj7s73ak5pd0-a.ohio-postgres.render.com",
+    "dialect": "postgres"
+  }
+}
 
-// Configuração do EJS
-app.engine('ejs', engine);
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
 
-// Middleware para parsear dados do corpo da requisição
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Middleware para sobrescrever métodos HTTP
-app.use(methodOverride('_method'));
-
-// Configuração das rotas
-const homeRoutes = require('./routes/homeRoutes');
-const artistaRoutes = require('./routes/artistaRoutes');
-const discoRoutes = require('./routes/discoRoutes');
-const faixaRoutes = require('./routes/faixaRoutes');
-const generoRoutes = require('./routes/generoRoutes');
-const vincularArtistaRoutes = require('./routes/vincularArtistaRoutes')
-
-// Definindo as rotas principais
-app.use('/', homeRoutes);
-app.use('/artistas', artistaRoutes);
-app.use('/discos', discoRoutes);
-app.use('/faixas', faixaRoutes);
-app.use('/generos', generoRoutes);
-app.use('/vincular-artista', vincularArtistaRoutes)
-
-// Serve os arquivos estáticos do frontend (caso haja)
-app.use(express.static(path.join(__dirname, 'public')));
-// Servindo imagens como arquivos estáticos
-app.use('/images', express.static(path.join(__dirname, 'images')));
-
-// Configuração de erros 404
-app.use((req, res, next) => {
-  res.status(404).send('Página não encontrada!');
-});
-
-// Inicialização do banco de dados e o servidor
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT = 3000, () => {
-    console.log(`Servidor rodando na porta http://localhost:${PORT}`);
-  });
-});
