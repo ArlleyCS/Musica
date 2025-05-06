faaead70c7530cb140f394588a21f0805a671377
ArlleyCS <arllinho@edu.unifil.br>
2025-05-05 22:08:50 -0300

    up
    2

--- app.js
+++ app.js
@@ -1,26 +1,18 @@
-{
-  "development": {
-    "username": "baianosound_user",
-    "password": "oCs0Q1YPj1O8KoNVI6Zdtmrm9NNGnFvl",
-    "database": "baianosound",
-    "host": "dpg-d0cigdumcj7s73ak5pd0-a.ohio-postgres.render.com",
-    "dialect": "postgres",
-    "logging": false
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "baianosound_user",
-    "password": "oCs0Q1YPj1O8KoNVI6Zdtmrm9NNGnFvl",
-    "database": "baianosound",
-    "host": "dpg-d0cigdumcj7s73ak5pd0-a.ohio-postgres.render.com",
-    "dialect": "postgres"
-  }
-}
+const config = require('./config.json'); // Ajuste o caminho se necessário
+const { Sequelize } = require('sequelize');
 
+// Crie uma instância do Sequelize com as configurações do arquivo config.json
+const sequelize = new Sequelize(config.development.database, config.development.username, config.development.password, {
+  host: config.development.host,
+  dialect: config.development.dialect,
+  logging: config.development.logging,
+});
 
+// Teste a conexão com o banco de dados
+sequelize.authenticate()
+  .then(() => {
+    console.log('Conexão com o banco de dados estabelecida com sucesso!');
+  })
+  .catch(err => {
+    console.error('Não foi possível conectar ao banco de dados:', err);
+  });
